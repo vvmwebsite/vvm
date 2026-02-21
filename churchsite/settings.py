4ab325eca7eeb39dfcc68a40c6377385e6ee494d
vvmwebsite <tnvvmwebsit@gmail.com>
2026-02-21 21:14:48 +0530

    vvbnm,

--- churchsite/settings.py
+++ churchsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('cplu+x$^mh-a!b1^_blr!q1($!!s)d#!(6yk+txb81btjm24)', 'fallback-secret-key-for-dev')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
