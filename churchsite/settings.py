b472c7995111cccdbb7ce250fe429b2b5076c53a
vvmwebsite <tnvvmwebsite@gmail.com>
2026-02-21 23:30:15 +0530

    Update settings.py

--- churchsite/settings.py
+++ churchsite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['churchsite.onrender.com','villagevissionministries.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
