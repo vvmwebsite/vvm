59b3f99d4decbd77bd5c83df5a282e8cda51f679
vvmwebsite <tnvvmwebsit@gmail.com>
2026-02-21 23:05:46 +0530

    wertg

--- churchsite/settings.py
+++ churchsite/settings.py
@@ -75,16 +75,17 @@ WSGI_APPLICATION = 'churchsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('vvm_db_10vt'),
-        'USER': os.environ.get('vvm_db_10vt_user'),
-        'PASSWORD': os.environ.get('Jg2S1TMkxSC4PteIkArqWGm8pEEUscj1'),
-        'HOST': os.environ.get('dpg-d6csiqsr85hc73bf2dl0-a'),
-        'PORT': os.environ.get('5432'),
+if os.environ.get("RENDER"):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
