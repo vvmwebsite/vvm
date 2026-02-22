3354b878777292a80f9af769c4b1ae4657142727
vvmwebsite <tnvvmwebsit@gmail.com>
2026-02-22 08:09:52 +0530

    wertxc

--- churchsite/urls.py
+++ churchsite/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('vvm-admin-622000/', admin.site.urls),
     path('', include('mainapp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
